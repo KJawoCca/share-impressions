3b502686f69fece00553bc7d10a466b9aa954333
KJawoCca <ykariya.pri@gmail.com>
2020-12-11 06:30:39 +0900

    テキスト投稿機能実装

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,8 +7,17 @@ class ReviewsController < ApplicationController
   end
 
   def create
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
-  
+  def review_params
+    params.require(:review).permit(:image, :title, :opus_title, :author_name, :authors_website, :text).merge(user_id: current_user.id)
+  end
+
 end
