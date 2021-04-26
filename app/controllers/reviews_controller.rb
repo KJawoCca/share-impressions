db50a12359afe1372071789901ca45aff3c4f732
KJawoCca <ykariya.pri@gmail.com>
2021-04-26 13:59:08 +0900

    編集画面作成2

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,7 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :find_review, only: [:show, :edit, :update]
+
   def index
     @reviews = Review.order("created_at DESC")
   end
@@ -17,16 +20,26 @@ class ReviewsController < ApplicationController
   end
 
   def show
-    @review = Review.find(params[:id])
   end
 
   def edit
-    @review = Review.find(params[:id])
   end
 
+  def update
+    if @review.update(review_params)
+      redirect_to edit_review_path(params[:id])
+    else
+      render :edit
+    end
+  end
+  
   private
   def review_params
     params.require(:review).permit(:image, :title, :opus_title, :author_name, :authors_website, :text).merge(user_id: current_user.id)
+  end
+
+  def find_review
+    @review = Review.find(params[:id])
   end
 
 end
