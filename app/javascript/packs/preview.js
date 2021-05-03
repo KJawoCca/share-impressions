03b6dc09f241a99fb8b31e81d5564997fa049920
KJawoCca <ykariya.pri@gmail.com>
2021-05-03 15:45:58 +0900

    プレビュー機能実装

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -5,16 +5,17 @@ if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
     document.getElementById('opus_image').addEventListener('change', function(e){
       // ユーザーが投稿した画像のプレビューが表示されている場合、すでに存在している画像と説明文を削除する
       const imageContent = document.querySelector('img');
-      const imageText = document.getElementById('image_text');
       const imageTitle = document.getElementById('image_title');
       if (imageContent){
         imageContent.remove();
+      }
+      if (imageTitle){
         imageTitle.textContent = "";
       }
-
+      // 選択された画像情報を取得しURLを生成する
       const file = e.target.files[0];
       const blob = window.URL.createObjectURL(file);
-
+      // 画像表示のためのHTML要素を生成して反映させる
       const imageElement = document.createElement('div');
 
       const blobImage = document.createElement('img');
